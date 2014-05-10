c703406d4273be7bb936a14ad91052798dd4bc29
Wei Hsu <yayitswei@gmail.com>
2014-05-10 21:19:38 +0800

    better resizing

--- src/tedit/core.cljs
+++ src/tedit/core.cljs
@@ -9,7 +9,6 @@
 
 ;; mouse events
 
-;; Overcome some of the browser limitations around DnD
 (def mouse-move-ch
   (chan (sliding-buffer 1)))
 
@@ -25,25 +24,25 @@
 
 ;; state
 
-(def config {:canvas-width 500
-             :canvas-height 500})
+(defonce config {:canvas-width 500
+                 :canvas-height 750})
 
-(def component (atom {:x 100 :y 100
-                      :height 50 :width 200
-                      :text "test long text here we go"
-                      :dragging false :rel-x 0 :rel-y 0
-                      :highlight true}))
+(defonce component (atom {:x 250 :y 100
+                          :text "test long text here we go"
+                          :dragging false
+                          :font-size 25
+                          :highlight true}))
 
-(def comms {:mouse-move (async/mult mouse-move-ch)
-            :mouse-down (async/mult mouse-down-ch)
-            :mouse-up (async/mult mouse-up-ch)})
+(defonce comms {:mouse-move (async/mult mouse-move-ch)
+                :mouse-down (async/mult mouse-down-ch)
+                :mouse-up (async/mult mouse-up-ch)})
 
-(def transients (atom {}))
+(defonce transients (atom {}))
 
-(def highlighted (atom true))
-(def selected (atom false))
+(defonce highlighted (atom true))
+(defonce selected (atom false))
 
-(def debug (atom {}))
+(defonce debug (atom {}))
 
 ;; drag fns
 
@@ -56,6 +55,32 @@
                       :elem-start-state @component})
   (swap! component merge {:dragging true}))
 
+(defn transform-matrix [elem]
+  [(:scale-x elem 1) 0 0 (:scale-y elem 1) (:x elem 0) (:y elem 0)])
+
+(defn transform-str [& args]
+  (str "matrix(" (clojure.string/join "," args) ")"))
+
+(defn get-element [id] (. js/document (getElementById id)))
+
+(def text-padding 20)
+
+(defn text-measurements [elem]
+  (let [text-node (get-element "measure-text")
+        tspan-node (get-element "measure-tspan")
+        _ (.setAttribute text-node "font-size" (:font-size elem))
+        _ (aset tspan-node "textContent" (:text elem))
+        bb (.getBBox text-node)
+        bounding-box {:width (.-width bb) :height (.-height bb)}]
+    bounding-box))
+
+(defn text-bounding-box [elem]
+  (let [m (text-measurements elem)]
+    {:x (- (:x elem) (/ (:width m) 2))
+     :y (- (:y elem) (:height m))
+     :width (:width m)
+     :height (* (:height m) 1.2)}))
+
 (defn constrain [v min-v max-v]
   (cond (< v min-v) min-v
         (> v max-v) max-v
@@ -63,17 +88,18 @@
 
 (defn e->translation [e {:keys [mouse-start-state elem-start-state]}]
   (let [[delta-x delta-y] (mouse-delta e mouse-start-state)]
-    {:x (constrain (+ (:x elem-start-state) delta-x)
-                   (- (/ (:width elem-start-state) 2))
-                   (- (:canvas-width config) (/ (:width elem-start-state) 2)))
-     :y (constrain (+ (:y elem-start-state) delta-y)
-                   (- (/ (:height elem-start-state) 2))
-                   (- (:canvas-height config) (/ (:height elem-start-state) 2)))}))
+    {:x (constrain (+ (:x elem-start-state) delta-x) 0 (:canvas-width config))
+     :y (constrain (+ (:y elem-start-state) delta-y) 0 (:canvas-height config))}))
+
+(defn dominant [f values]
+  (apply (partial max-key Math/abs) values))
 
 (defn e->scale [e {:keys [mouse-start-state elem-start-state]}]
-  (let [[delta-x delta-y] (mouse-delta e mouse-start-state)]
-    {:height (+ (:height elem-start-state) delta-y)
-     :width (+ (:width elem-start-state) delta-x)}))
+  (let [deltas (mouse-delta e mouse-start-state)
+        new-font-size (max (+ (:font-size elem-start-state) (dominant Math/abs deltas)) 10)]
+    #_(reset! debug {:font-size new-font-size :deltas (print-str deltas)
+                   :dominant-delta (dominant Math/abs deltas)})
+    {:font-size new-font-size}))
 
 (defn drag-move [e]
   (when (:dragging @component)
@@ -96,61 +122,6 @@
   (when (:resizing @component)
     (swap! component merge (e->scale e @transients) {:resizing false})))
 
-(defn resize-button []
-  (let [move (async/tap (:mouse-move comms) (chan))
-        up (async/tap (:mouse-up comms) (chan))]
-    (go (while true
-          (alt! move ([e] (resize-move e))
-                up ([e] (resize-end e)))))
-    (fn []
-      [:image {:on-mouse-down (fn [e]
-                                (.preventDefault e)
-                                (.stopPropagation e)
-                                (resize-start e))
-
-               :on-mouse-up (fn [e] (resize-end e))
-
-               :xlink-href "scale.png"
-
-               :x (+ (:x @component) (:width @component))
-               :y (+ (:y @component) (:height @component))
-               :height 20 :width 20}])))
-
-(defn transform-matrix [elem]
-  [(:scale-x elem 1) 0 0 (:scale-y elem 1) (:x elem 0) (:y elem 0)])
-
-(defn transform-str [& args]
-  (str "matrix(" (clojure.string/join "," args) ")"))
-
-(defn get-element [id] (. js/document (getElementById id)))
-
-(def text-padding 20)
-
-(defn text-transform [text width height]
-  (let [text-node (get-element "measure-text")
-        _ (aset text-node "textContent" text)
-        bb (.getBBox text-node)
-        width-transform (/ (- width text-padding) (.-width bb))
-        height-transform (/ (- height text-padding) (.-height bb))
-        scale-value (min width-transform height-transform)
-        new-width (* scale-value (.-width bb))
-        new-height (* scale-value (.-height bb))
-        x-offset (/ width 2)
-        y-offset (+ (/ height 2) (/ new-height 2))]
-    (reset! debug {:width-transform width-transform
-                   :height-transform height-transform
-                   :new-width new-width
-                   :new-height new-height
-                   :x-offset x-offset
-                   :y-offset y-offset})
-    {:scale-x scale-value
-     :scale-y scale-value
-     :x x-offset
-     :y y-offset}))
-
-(defn text-transform-str [{:keys [width height text]}]
-  (apply transform-str (transform-matrix (text-transform text width height))))
-
 (defn debug-component []
   [:div
    (for [[prop v] @debug]
@@ -167,54 +138,65 @@
 
     (fn []
       [:g
-       [:svg {
-              :on-mouse-down (fn [e]
-                               (.preventDefault e)
-                               (.stopPropagation e)
-                               (drag-start e))
+       [:text {:on-mouse-down (fn [e]
+                                (.preventDefault e)
+                                (.stopPropagation e)
+                                (drag-start e))
 
-              :on-mouse-up (fn [e]
-                             (drag-end e)
-                             (reset! selected true))
+               :on-mouse-up (fn [e]
+                              (drag-end e)
+                              (reset! selected true))
 
-              :on-mouse-over (fn [e] (reset! highlighted true))
-              :on-mouse-out (fn [e] (reset! highlighted false))
+               :on-mouse-over (fn [e] (reset! highlighted true))
+               :on-mouse-out (fn [e] (reset! highlighted false))
 
-              :x (:x @component)
-              :y (:y @component)
-              :text-anchor "middle"
-              :height (:height @component)
-              :width (:width @component)
-              :style {:border "black"}}
-        [:text {:transform (text-transform-str @component)
-                :dominant-baseline "hanging"} (:text @component)]]])))
+               :x (:x @component)
+               :y (:y @component)
+               :text-anchor "middle"
+               :height (:height @component)
+               :width (:width @component)
+               :style {:border "black"
+                       :font-size (:font-size @component)}}
+        [:tspan (:text @component)]]])))
+
+
+(defn resize-button []
+  (let [move (async/tap (:mouse-move comms) (chan))
+        up (async/tap (:mouse-up comms) (chan))]
+    (go (while true
+          (alt! move ([e] (resize-move e))
+                up ([e] (resize-end e)))))
+    (fn []
+      (let [bb (text-bounding-box @component)]
+        [:image {:on-mouse-down (fn [e]
+                                  (.preventDefault e)
+                                  (.stopPropagation e)
+                                  (resize-start e))
+
+                 :on-mouse-up (fn [e] (resize-end e))
+
+                 :xlink-href "scale.png"
+
+                 :x (+ (:x bb) (:width bb))
+                 :y (+ (:y bb) (:height bb))
+                 :height 20 :width 20}]))))
 
 (defn controls-component []
   (fn [] [:g
-          [:rect {:fill :none
-                  :stroke-dasharray "4,3"
-                  :stroke "#000000"
-                  :x (:x @component)
-                  :y (:y @component)
-                  :width (:width @component)
-                  :height (:height @component)
-;          :style {:pointer-events :none}
-                  }]
+          [:rect (merge (text-bounding-box @component)
+                        {:fill :none
+                         :stroke-dasharray "4,3"
+                         :stroke "#000000"})]
           [resize-button]]))
 
 (defn highlight-box []
-  [:rect {:fill :none
-          :stroke "#000000"
-          :x (:x @component)
-          :y (:y @component)
-          :width (:width @component)
-          :height (:height @component)
-;          :style {:pointer-events :none}
-          }])
+  [:rect (merge (text-bounding-box @component)
+                {:fill :none
+                 :stroke "#000000"})])
 
 (defn canvas-component []
   [:div
-   [:h1 "TEdit"]
+   [:h1 "tEdit"]
    [debug-component]
    [:div {:style {:border "1px solid black" :display :inline-block}}
     [:svg {:width (:canvas-width config) :height (:canvas-height config)

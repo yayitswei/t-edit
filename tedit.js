ec83a46017e634efcfaa818d39bca98d6a9ba66a
Wei Hsu <yayitswei@gmail.com>
2014-05-13 23:20:13 +0800

    centered art!

--- tedit.js
+++ tedit.js
@@ -16,4 +16,4 @@ goog.addDependency("../cljs/core/async/impl/dispatch.js", ['cljs.core.async.impl
 goog.addDependency("../cljs/core/async/impl/channels.js", ['cljs.core.async.impl.channels'], ['cljs.core.async.impl.buffers', 'cljs.core', 'cljs.core.async.impl.dispatch', 'cljs.core.async.impl.protocols']);
 goog.addDependency("../cljs/core/async/impl/timers.js", ['cljs.core.async.impl.timers'], ['cljs.core', 'cljs.core.async.impl.channels', 'cljs.core.async.impl.dispatch', 'cljs.core.async.impl.protocols']);
 goog.addDependency("../cljs/core/async.js", ['cljs.core.async'], ['cljs.core.async.impl.ioc_helpers', 'cljs.core.async.impl.buffers', 'cljs.core', 'cljs.core.async.impl.channels', 'cljs.core.async.impl.dispatch', 'cljs.core.async.impl.protocols', 'cljs.core.async.impl.timers']);
-goog.addDependency("../tedit/core.js", ['tedit.core'], ['cljs.core', 'reagent.core', 'clojure.string', 'cljs.core.async']);
+goog.addDependency("../tedit/core.js", ['tedit.core'], ['cljs.core', 'goog.array', 'goog.dom', 'reagent.core', 'clojure.string', 'cljs.core.async']);
